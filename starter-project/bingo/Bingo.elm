0c8f42472263383acc7a53b2ce64984dac5f9c66
jnyinglis <john.inglis@me.com>
2017-07-09 19:28:46 -0400

    Submitting scores

--- starter-project/bingo/Bingo.elm
+++ starter-project/bingo/Bingo.elm
@@ -3,11 +3,19 @@ module Bingo exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Random
+import Http
+import Json.Decode as Decode exposing (Decoder, field, succeed)
+import Json.Encode as Encode
 
 -- MODEL
 
 type alias Model =
-    { name : String, gameNumber : Int, entries : List Entry }
+    { name : String
+    , gameNumber : Int
+    , entries : List Entry
+    , alertMessage : Maybe String
+    }
 
 type alias Entry = 
     { id : Int
@@ -16,42 +24,205 @@ type alias Entry =
     , marked : Bool
     }
 
+type alias Score =
+    { id : Int
+    , name : String
+    , score : Int
+    }
+
 initialModel : Model
 initialModel =
     { name = "John"
     , gameNumber = 1
-    , entries = initialEntries
+    , entries = []
+    , alertMessage = Maybe.Nothing
+--    , entries = initialEntries
     }
 
-initialEntries : List Entry
-initialEntries =
-    [ Entry 1 "Future-Proof" 100 False -- { id = 1, phrase = "Future-proof", points = 100, marked = False }
-    , Entry 2 "Doing Agile" 200 False -- { id = 2, phrase = "Doing Agile", points = 200, marked = False } 
-    , Entry 3 "In The Cloud" 300 False
-    , Entry 4 "Rock-Star Ninja" 400 False
-    ]
+--initialEntries : List Entry
+--initialEntries =
+--    [ Entry 1 "Future-Proof" 100 False -- { id = 1, phrase = "Future-proof", points = 100, marked = False }
+--    , Entry 2 "Doing Agile" 200 False -- { id = 2, phrase = "Doing Agile", points = 200, marked = False } 
+--    , Entry 3 "In The Cloud" 300 False
+--    , Entry 4 "Rock-Star Ninja" 400 False
+--    ]
 
 -- UPDATE
 
-type Msg = NewGame | Mark Int
+type Msg
+    = NewGame
+    | Mark Int
+    | NewRandom Int
+    | NewEntries (Result Http.Error (List Entry))
+    | CloseAlert
+    | ShareScore
+    | NewScore (Result Http.Error Score)
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NewGame ->
-            { model | gameNumber = model.gameNumber + 1,
-                entries = initialEntries }
-        Mark id ->
-            let
-                markEntry e =
-                    if e.id == id then
-                        { e | marked = (not e.marked) }
-                    else
-                        e
-            in
-               { model | entries = List.map markEntry model.entries } 
+    let
+        sortByDesc a list = List.reverse (List.sortBy a list)
+    in
+        case msg of
+            NewRandom randomNumber ->
+                ( { model | gameNumber = randomNumber }, Cmd.none )
+    
+            ShareScore ->
+                ( model, postScore model )
+
+            NewScore (Ok score) ->
+                let
+                    message =
+                        "Your score of " 
+                            ++ (toString score.score)
+                            ++ "was successfully shared!"
+                in
+                    ( { model | alertMessage = Just "Success!" }, Cmd.none )
+                    
+            NewScore (Err error) ->
+                let
+                    message =
+                        "Error posting your score: " 
+                            ++ (toString error)
+                in
+                    ( { model | alertMessage = Just "Success!" }, Cmd.none )
+
+            NewGame ->
+                ( { model | gameNumber = model.gameNumber + 1 }, getEntries)
+    
+            NewEntries (Ok randomEntries) ->
+                ( { model | entries = sortByDesc .points randomEntries }, Cmd.none )
+    
+            NewEntries (Err error) ->
+                let
+                    errorMessage =
+                        case error of
+                            Http.NetworkError ->
+                                "Is the server running?"
+
+                            Http.BadStatus response ->
+                                (toString response.status)
+
+                            Http.BadPayload message _ ->
+                                "Decoding Failed: " ++ message
+
+                            _ ->
+                                (toString error)
+                in
+                    ( { model | alertMessage = Just errorMessage }, Cmd.none)
+    
+            CloseAlert ->
+                ( { model | alertMessage = Maybe.Nothing }, Cmd.none )
+            Mark id ->
+                let
+                    markEntry e =
+                        if e.id == id then
+                            { e | marked = (not e.marked) }
+                        else
+                            e
+                in
+                ( { model | entries = List.map markEntry model.entries }, Cmd.none )
+
+-- DECODERS/ENCODERS
+
+entryDecoder : Decoder Entry
+entryDecoder =
+    Decode.map4 Entry
+        (field "id" Decode.int)
+        (field "phrase" Decode.string)
+        (field "points" Decode.int)
+        (succeed False)
+
+scoreDecoder : Decoder Score
+scoreDecoder =
+    Decode.map3 Score
+        (field "id" Decode.int)
+        (field "name" Decode.string)
+        (field "score" Decode.int)
+
+encodeScore : Model -> Encode.Value
+encodeScore model =
+            Encode.object
+                [ ("name", Encode.string model.name)
+                , ("score", Encode.int (sumMarkedPoints model.entries))
+                ]
+
+-- COMMANDS
+
+generateRandomNumber : Cmd Msg
+generateRandomNumber =
+--    Random.generate (\num -> NewRandom num) (Random.int 1 100)
+    Random.generate NewRandom (Random.int 1 100)
+
+apiUrlPrefix : String
+apiUrlPrefix =
+    "http://localhost:3000"
+
+entriesUrl : String
+entriesUrl =
+    apiUrlPrefix ++ "/random-entries"
+
+postScoresUrl : String
+postScoresUrl =
+    apiUrlPrefix ++ "/scores"
+
+postScore : Model -> Cmd Msg
+postScore model =
+    let
+        url =
+            postScoresUrl
+
+        body =
+            encodeScore model
+                |> Http.jsonBody
+
+        request =
+            Http.post url body scoreDecoder
+    in
+        Http.send NewScore request
+
+getEntries : Cmd Msg
+getEntries =
+--    Http.send NewEntries (Http.getString entriesUrl)
+    (Decode.list entryDecoder)
+        |> Http.get entriesUrl
+        |> Http.send NewEntries
+
+    -- send : (Result Http.Error String -> Msg) -> Request String -> Cmd Msg
 
 -- VIEW
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer model.name model.gameNumber
+        , viewAlertMessage model.alertMessage
+        , viewEntryList model.entries
+        , viewScore (sumMarkedPoints model.entries)
+        , div [ class "button-group" ]
+              [ button [ onClick NewGame ] [ text "New Game" ]
+              , button [ onClick ShareScore ] [ text "Share Score"]
+              ]
+        , div [ class "debug" ] [ text (toString model) ]
+        , viewFooter
+        ]
+
+--main : Html Msg
+--main =
+--    view initialModel
+
+viewAlertMessage : Maybe String -> Html Msg
+viewAlertMessage alertMessage =
+    case alertMessage of
+        Just message ->
+            div [ class "alert" ]
+                [ span [ class "close", onClick CloseAlert ] [ text "X" ]
+                , text message
+                ]
+        
+        Maybe.Nothing ->
+            text ""
 
 playerInfo : String -> Int -> String
 playerInfo name gameNumber =
@@ -116,28 +287,12 @@ viewScore sum =
         , span [ class "value" ] [ text (toString sum) ]
         ]
 
-view : Model -> Html Msg
-view model =
-    div [ class "content" ]
-        [ viewHeader "BUZZWORD BINGO"
-        , viewPlayer model.name model.gameNumber
-        , viewEntryList model.entries
-        , viewScore (sumMarkedPoints model.entries)
-        , div [ class "button-group" ]
-              [ button [ onClick NewGame ] [ text "New Game" ] ]
-        , div [ class "debug" ] [ text (toString model) ]
-        , viewFooter
-        ]
-
---main : Html Msg
---main =
---    view initialModel
-
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = initialModel
+    Html.program
+        { init = ( initialModel, getEntries )
         , view = view
         , update = update
+        , subscriptions = (\_ -> Sub.none )
         }
                     
